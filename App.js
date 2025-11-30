fdda4ec4fc8a534e82318d9d3bf0de4ae0e74cb9
Javier Dibo Gómez <javier.fco.dibo.gomez@gmail.com>
2025-11-30 01:55:10 +0100

    It works

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { Text, View } from 'react-native';
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold">Esta es un template con Tailwind</Text>
+      <Text className="text-xl font-bold text-red-300 p-10 text-center">Esta es un template con Tailwind funcionando</Text>
       <StatusBar style="auto" />
     </View>
   );
