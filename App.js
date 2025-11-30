55583c2506941f499451dd856e69639a8d77cab0
Javier Dibo Gómez <javier.fco.dibo.gomez@gmail.com>
2025-11-30 01:40:25 +0100

    Tailwind working

--- App.js
+++ App.js
@@ -1,57 +1,12 @@
-import * as React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-
-function HomeScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      <Button
-        title="Go to Details"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
-  );
-}
-
-function DetailsScreen({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Details Screen</Text>
-      <Button
-        title="Go to Home"
-        onPress={() => navigation.navigate('Home')}
-      />
-      <Button
-        title="Go back"
-        onPress={() => navigation.goBack()}
-      />
-    </View>
-  );
-}
-
-const Stack = createNativeStackNavigator();
+import './global.css';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-xl font-bold">Esta es un template con Tailwind</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 24,
-    marginBottom: 20,
-  },
-});
