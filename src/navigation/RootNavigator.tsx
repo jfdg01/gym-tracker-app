a88115ecbe97b25b8ad43327928f188b69aad0ed
Javier Dibo Gómez <javier.fco.dibo.gomez@gmail.com>
2025-12-02 19:13:55 +0100

    removed unused screen

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { DebugDashboardScreen } from '../screens/DebugDashboardScreen';
+
 import { TableListScreen } from '../screens/TableListScreen';
 import { EditRecordScreen } from '../screens/EditRecordScreen';
 import { ActiveExerciseScreen } from '../screens/ActiveExerciseScreen';
@@ -23,7 +23,6 @@ export const RootNavigator = () => {
                 <NavigationContainer>
                     <Stack.Navigator initialRouteName="Tabs" id="RootStack">
                         <Stack.Screen name="Tabs" component={TabNavigator} options={{ headerShown: false }} />
-                        <Stack.Screen name="DebugDashboard" component={DebugDashboardScreen} options={{ title: 'Debug Dashboard' }} />
                         <Stack.Screen name="TableList" component={TableListScreen} options={({ route }: any) => ({ title: route.params.tableName })} />
                         <Stack.Screen name="EditRecord" component={EditRecordScreen} options={({ route }: any) => ({ title: route.params.recordId ? 'Edit Record' : 'Add Record' })} />
                         <Stack.Screen name="ActiveExercise" component={ActiveExerciseScreen} options={{ headerShown: false }} />
